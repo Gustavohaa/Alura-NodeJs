32180439c89c6bc62dae280d5e66409cdc734d07
Gustavohaa <111452073+Gustavohaa@users.noreply.github.com>
2024-11-21 16:52:33 -0300

    Class 03

--- server.js
+++ server.js
@@ -1,45 +1,10 @@
-import express from 'express';
-
-const posts = [
-    {   id: 1,
-        descricao: "Foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-      },
-      {
-        id: 2,
-        descricao: "Gato fazendo yoga",
-        imagem: "https://placekitten.com/400/300"
-      },
-      {
-        id: 3,
-        descricao: "Gatinho dormindo",
-        imagem: "https://placekitten.com/200/200"
-      },
-      {
-        id: 4,
-        descricao: "Cachorro brincando com bola",
-        imagem: "https://placepup.com/500/400"
-      }
-];
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000');
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function getPostById(id){
-    return posts.findIndex((post)=>{
-        return post.id === Number(id)
-    });
-};
-
-app.get("/posts/:id", (req, res) => {
-    const index = getPostById(req.params.id);
-    res.status(200).json(posts[index]);
+    console.log("Servidor escutando...");
 });
---
+++ src/controllers/postController.js
@@ -0,0 +1,8 @@
+import { getAllPosts } from "../models/postsModel.js";
+
+export async function listPosts(req, res) {
+
+    const posts = await getAllPosts();
+     
+    res.status(200).json(posts); 
+};
